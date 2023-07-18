bb0a954c867a821ef22b7c9c92be4f2d35785b38
belhassen <belhassenkhefacha@gmail.com>
2023-07-18 09:42:02 +0100

    projet

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
+use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IngredientController extends AbstractController
+{
+    /**
+     * This function display all ingredients
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'ingredient.index',methods: ['GET'])]
+    public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    {
+        $ingredients = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page',1),
+            10
+        );
+        return $this->render('pages/ingredient/index.html.twig', [
+            'ingredients' => $ingredients
+        ]);
+    }
+
+    #[Route('/ingredient/nouveau', 'ingredient.new' , methods: ['GET','POST'])]
+
+    public function new(Request $request, EntityManagerInterface $manager): Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $ingredient = $form->getData();
+            $manager->persist($ingredient);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'Votre ingredient a été crée avec succées'
+            );
+            return $this->redirectToRoute('ingredient.index');
+        }
+        return $this->render('pages/ingredient/new.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
+
+    public function
+}
