5bf45c94ba3d0c23cf4d6325994c64ffda147643
belhassen <belhassenkhefacha@gmail.com>
2023-07-18 10:34:57 +0100

    projet

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -7,7 +7,6 @@ use App\Entity\Ingredient;
 use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Mapping\Entity;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,7 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class IngredientController extends AbstractController
 {
     /**
-     * This function display all ingredients
+     * This controller display all ingredients
      *
      * @param IngredientRepository $repository
      * @param PaginatorInterface $paginator
@@ -37,6 +36,13 @@ class IngredientController extends AbstractController
         ]);
     }
 
+    /**
+     * This controller show a form which create an ingredient
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
     #[Route('/ingredient/nouveau', 'ingredient.new' , methods: ['GET','POST'])]
 
     public function new(Request $request, EntityManagerInterface $manager): Response
@@ -59,5 +65,23 @@ class IngredientController extends AbstractController
         ]);
     }
 
-    public function
+    #[Route('/ingredient/edition/{id}','ingredient.edit',methods: ['GET','POST'])]
+    public function edit(Ingredient $ingredient, Request $request,EntityManagerInterface $manager):Response
+    {
+        $form = $this->createForm(IngredientType::class, $ingredient);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $ingredient = $form->getData();
+            $manager->persist($ingredient);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'Votre ingredient a été crée avec succées'
+            );
+            return $this->redirectToRoute('ingredient.index');
+        }
+        return $this->render('pages/ingredient/edit.html.twig',[
+            'form' => $form->createView()
+        ]);
+    }
 }
