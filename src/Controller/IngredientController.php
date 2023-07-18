ff9b80bcf76cd4b1d23de34b09be55248b4d10c9
belhassen <belhassenkhefacha@gmail.com>
2023-07-18 14:15:46 +0100

    CRUD ingredients

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -65,9 +65,16 @@ class IngredientController extends AbstractController
         ]);
     }
 
-    #[Route('/ingredient/edition/{id}','ingredient.edit',methods: ['GET','POST'])]
-    public function edit(Ingredient $ingredient, Request $request,EntityManagerInterface $manager):Response
+    /**
+     * @param Ingredient $ingredient
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route('/ingredient/edition/{id}', 'ingredient.edit', methods: ['GET','POST'])]
+    public function edit( Request $request, EntityManagerInterface $manager,$id): Response
     {
+        $ingredient = $manager->getRepository(Ingredient::class)->find($id);
         $form = $this->createForm(IngredientType::class, $ingredient);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
@@ -76,12 +83,24 @@ class IngredientController extends AbstractController
             $manager->flush();
             $this->addFlash(
                 'success',
-                'Votre ingredient a été crée avec succées'
+                'Votre ingredient a été modifié avec succées'
             );
             return $this->redirectToRoute('ingredient.index');
         }
         return $this->render('pages/ingredient/edit.html.twig',[
             'form' => $form->createView()
         ]);
+    }
+
+    #[Route('/ingredient/suppression/{id}','ingredient.delete',methods: ['GET'])]
+    public function delete(EntityManagerInterface $manager,$id):Response
+    {
+        $ingredient = $manager->getRepository(Ingredient::class)->find($id);
+        $manager->remove($ingredient);
+        $manager->flush();
+        $this->addFlash(
+            'success', 'Votre ingredient a été supprimé avec succées');
+
+        return $this->redirectToRoute('ingredient.index');
     }
 }
